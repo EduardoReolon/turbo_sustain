04a95a25999bd2f80ed5fc5825910bfb117e1eaf
Eduardo <eduardo_reolon@hotmail.com>
2026-06-24 10:18:33 -0300

    ajustes

--- run.bat
+++ run.bat
@@ -1,20 +1,7 @@
 @echo off
-set VENV_DIR=venv
-
-REM Criar venv se não existir
-if not exist %VENV_DIR% (
-    echo 🔹 Criando ambiente virtual...
-    python -m venv %VENV_DIR%
-    call %VENV_DIR%\Scripts\activate
-    pip install --upgrade pip
-    pip install -r requirements.txt
-) else (
-    echo 🔹 Ativando ambiente virtual...
-    call %VENV_DIR%\Scripts\activate
-)
 
 REM Executar script
-echo 🔹 Iniciando teste...
+echo Iniciando teste...
 python main.py
 
 REM Mantém a janela aberta após o servidor ser interrompido (Ctrl+C)
